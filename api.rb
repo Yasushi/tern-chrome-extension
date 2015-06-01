c29f999393c77bdeae0a6f7a8a64d0852ad5773f
Yasushi Abe <yasushi.abe@gmail.com>
2015-06-01 21:40:16 +0900

    Generate chrome-extension.js directry and UMD-ized. #1

--- api.rb
+++ api.rb
@@ -115,4 +115,23 @@ defs.select{|d| d.is_a?(TEvent)}.each do |d|
   m["!url"]=d.link
 end
 
-open("chrome-extension.json","w"){|f| f.write(JSON.generate(r,indent:''))}
+open("chrome-extension.js","w"){|f|
+  f.write(DATA.read.sub(/DEFS/, JSON.generate(r,indent:'')))
+}
+
+__END__
+(function(mod) {
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+      return mod(require("tern/lib/infer"), require("tern/lib/tern"));
+  if (typeof define == "function" && define.amd) // AMD
+      return define([ "tern/lib/infer", "tern/lib/tern" ], mod);
+  mod(tern, tern);
+})(function(infer, tern) {
+  "use strict";
+
+  tern.registerPlugin("chrome-extension", function(server, options) {
+      return { defs : defs };
+  });
+
+  var defs = DEFS;
+});
